c419ef20b57c54ecdcc62782a68580b67d7724d3
kjetilja <kjetilja>
2001-08-27 14:48:09 +0000

    - bumped version number to 0.4.1

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-# $Id: setup.py,v 1.10 2001/08/20 10:49:13 kjetilja Exp $
+# $Id: setup.py,v 1.11 2001/08/27 14:48:09 kjetilja Exp $
 
 from distutils.core import setup, Extension
 
 setup(name="pycurl",
-      version="0.4.0",
+      version="0.4.1",
       description="PycURL -- cURL library module for Python",
       author="Kjetil Jacobsen",
       author_email="kjetilja@cs.uit.no",
