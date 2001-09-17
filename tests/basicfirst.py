6534cf2443117100ffb61b1df184b010a7b43a1e
kjetilja <kjetilja>
2001-09-17 12:55:37 +0000

    - added the basicfirst.pl example as python code instead

---
+++ tests/basicfirst.py
@@ -0,0 +1,24 @@
+# $Id: basicfirst.py,v 1.1 2001/09/17 12:55:37 kjetilja Exp $
+
+import sys
+import pycurl
+
+url = 'http://curl.haxx.se/dev/'
+contents = ''
+
+def body_callback(buf):
+    global contents
+    contents = contents + buf
+    return len(buf)
+
+print 'Testing curl version', pycurl.version 
+
+c = pycurl.init()
+c.setopt(pycurl.URL, url)
+c.setopt(pycurl.WRITEFUNCTION, body_callback)
+c.setopt(pycurl.HTTPHEADER, ["I-am-a-silly-programmer: yes indeed you are",
+                             "User-Agent: Python interface for libcURL"])
+c.perform()
+c.cleanup()
+
+print contents
