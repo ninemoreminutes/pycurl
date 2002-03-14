99d9f13076f5fca26b60b14f28d4c96a0c052d1e
kjetilja <kjetilja>
2002-03-14 16:34:52 +0000

    bumped version numbers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# $Id: setup.py,v 1.29 2002/02/27 11:20:07 kjetilja Exp $
+# $Id: setup.py,v 1.30 2002/03/14 16:34:52 kjetilja Exp $
 
 """Setup script for the PycURL module distribution."""
 
@@ -48,7 +48,7 @@ else:
 ###############################################################################
 
 setup (	name="pycurl",
-      	version="0.4.8",
+      	version="0.4.9",
       	description="PycURL -- cURL library module for Python",
       	author="Kjetil Jacobsen",
       	author_email="kjetilja@cs.uit.no",
