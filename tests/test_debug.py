a1d912eefaab16bfafea6fe1826fe5aeb11c62f7
kjetilja <kjetilja>
2002-04-15 12:02:59 +0000

    now works

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,9 +1,9 @@
-# $Id: test_debug.py,v 1.1 2002/04/15 11:05:59 kjetilja Exp $
+# $Id: test_debug.py,v 1.2 2002/04/15 12:02:59 kjetilja Exp $
 
 import pycurl
 
-def test(**args):
-    print args
+def test(t, b):
+    print "debug(%d): %s" % (t, b)
 
 c = pycurl.init()
 c.setopt(pycurl.URL, 'http://curl.haxx.se/')
