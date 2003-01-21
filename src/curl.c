d430bbea5b6ca73d240aa859ddb1dd21453ef5ef
mfx <mfx>
2003-01-21 16:06:18 +0000

    Undo a code cleanup. Fix possible memleak.

--- src/curl.c
+++ src/curl.c
@@ -1,4 +1,4 @@
-/* $Id: curl.c,v 1.190 2003/01/21 15:34:34 kjetilja Exp $ */
+/* $Id: curl.c,v 1.191 2003/01/21 16:06:18 mfx Exp $ */
 
 /* PycURL -- cURL Python module
  *
@@ -1996,7 +1996,8 @@ do_version_info(PyObject *dummy, PyObject *args)
         return NULL;
     }
 
-    for (i = 0; vi->protocols[i] != NULL; i++);
+    for (i = 0; vi->protocols[i] != NULL; )
+        i++;
     protocols = PyTuple_New(i);
     if (protocols == NULL) {
         return NULL;
@@ -2006,6 +2007,7 @@ do_version_info(PyObject *dummy, PyObject *args)
     }
     t = PyTuple_New(9);
     if (t == NULL) {
+        Py_DECREF(protocols);
         return NULL;
     }
 
