40d372325a0663652418b3f1fe26950c05eba8e0
mfx <mfx>
2003-04-16 05:18:58 +0000

    Updated.

--- setup_win32_ssl.py
+++ setup_win32_ssl.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
 # vi:ts=4:et
 
-# $Id: setup_win32_ssl.py,v 1.9 2003/02/25 19:05:51 mfx Exp $
+# $Id: setup_win32_ssl.py,v 1.10 2003/04/16 05:18:58 mfx Exp $
 
 import os, sys, string
 assert sys.platform == "win32", "Only for building on Win32 with SSL and zlib"
 
 
 CURL_DIR = r"c:\src\build\curl-7.10.4-ssl"
-OPENSSL_DIR = r"c:\src\build\openssl-0.9.6i"
+OPENSSL_DIR = r"c:\src\build\openssl-0.9.7b"
 ZLIB_DIR = r"c:\src\build\zlib-1.1.4"
 sys.argv.insert(1, "--curl-dir=" + CURL_DIR)
 
@@ -17,7 +17,7 @@ from setup import *
 setup_args["name"] = "pycurl-ssl"
 
 
-for l in ("RSAglue.lib", "libeay32.lib", "ssleay32.lib",):
+for l in ("libeay32.lib", "ssleay32.lib",):
     ext.extra_objects.append(os.path.join(OPENSSL_DIR, "out32", l))
 
 if 0:
