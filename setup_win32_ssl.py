d16270c4d6e90354611d14b6642ed70ac9388eb0
mfx <mfx>
2003-05-21 15:28:48 +0000

    Updates for 7.10.5 release.

--- setup_win32_ssl.py
+++ setup_win32_ssl.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # vi:ts=4:et
-# $Id: setup_win32_ssl.py,v 1.11 2003/04/21 18:46:09 mfx Exp $
+# $Id: setup_win32_ssl.py,v 1.12 2003/05/21 15:28:49 mfx Exp $
 
 import os, sys, string
 assert sys.platform == "win32", "Only for building on Win32 with SSL and zlib"
 
 
-CURL_DIR = r"c:\src\build\curl-7.10.4-ssl"
+CURL_DIR = r"c:\src\build\curl-7.10.5-ssl"
 OPENSSL_DIR = r"c:\src\build\openssl-0.9.7b"
 ZLIB_DIR = r"c:\src\build\zlib-1.1.4"
 sys.argv.insert(1, "--curl-dir=" + CURL_DIR)
--- src/pycurl.c
+++ src/pycurl.c
@@ -1,4 +1,4 @@
-/* $Id: pycurl.c,v 1.1 2003/05/15 13:47:47 mfx Exp $ */
+/* $Id: pycurl.c,v 1.2 2003/05/21 15:28:52 mfx Exp $ */
 
 /* PycURL -- cURL Python module
  *
@@ -57,11 +57,16 @@
 #undef UNUSED
 #define UNUSED(var)     ((void)&var)
 
+#undef COMPILE_TIME_ASSERT
+#define COMPILE_TIME_ASSERT(expr) \
+     { typedef int compile_time_assert_fail__[1 - 2 * !(expr)]; }
+
 
 /* Calculate the number of options we need to store */
 #define OPTIONS_SIZE    105
 static int PYCURL_OPT(int o)
 {
+    COMPILE_TIME_ASSERT(OPTIONS_SIZE == CURLOPT_HTTP200ALIASES - CURLOPTTYPE_OBJECTPOINT + 1);
     assert(o >= CURLOPTTYPE_OBJECTPOINT);
     assert(o < CURLOPTTYPE_OBJECTPOINT + OPTIONS_SIZE);
     return o - CURLOPTTYPE_OBJECTPOINT;
