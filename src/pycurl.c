de7240ee9c72d18834a3e0c5e066bbf56d7a3423
kjetilja <kjetilja>
2004-09-14 15:12:47 +0000

    curl_multi_add_handle returns CURLM_OK in newest libcurl (post 7.12.1)

--- src/pycurl.c
+++ src/pycurl.c
@@ -1,4 +1,4 @@
-/* $Id: pycurl.c,v 1.64 2004/08/17 15:16:57 mfx Exp $ */
+/* $Id: pycurl.c,v 1.65 2004/09/14 15:12:47 kjetilja Exp $ */
 
 /* PycURL -- cURL Python module
  *
@@ -1625,7 +1625,7 @@ do_multi_add_handle(CurlMultiObject *self, PyObject *args)
     }
     assert(obj->multi_stack == NULL);
     res = curl_multi_add_handle(self->multi_handle, obj->handle);
-    if (res != CURLM_CALL_MULTI_PERFORM) {
+    if (res != CURLM_OK) {
         CURLERROR_MSG("curl_multi_add_handle() failed due to internal errors");
     }
     obj->multi_stack = self;
