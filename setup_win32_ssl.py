152254b21e9364a576c6addbeff668f722aa5eaa
mfx <mfx>
2004-10-19 16:54:07 +0000

    Win32 updates.

--- setup_win32_ssl.py
+++ setup_win32_ssl.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # vi:ts=4:et
-# $Id: setup_win32_ssl.py,v 1.22 2004/10/15 08:56:22 kjetilja Exp $
+# $Id: setup_win32_ssl.py,v 1.23 2004/10/19 16:54:07 mfx Exp $
 
 import os, sys, string
 assert sys.platform == "win32", "Only for building on Win32 with SSL and zlib"
@@ -18,9 +18,9 @@ setup_args["name"] = "pycurl-ssl"
 
 for l in ("libeay32.lib", "ssleay32.lib",):
     ext.extra_objects.append(os.path.join(OPENSSL_DIR, "out32", l))
-ext.extra_objects.append(r"c:\src\pool\zlib-1.2.1.1\pool\win32\vc6\zlib.lib")
-ext.extra_objects.append(r"c:\src\build\pycurl\c-ares-1.2.0\ares.lib")
-ext.extra_objects.append(r"c:\src\pool\libidn-0.5.4\pool\win32\vc6\idn.lib")
+ext.extra_objects.append(r"c:\src\pool\zlib-1.2.2\pool\win32\vc6\zlib.lib")
+ext.extra_objects.append(r"c:\src\pool\c-ares-20041018\pool\win32\vc6\ares.lib")
+ext.extra_objects.append(r"c:\src\pool\libidn-0.5.8\pool\win32\vc6\idn.lib")
 
 
 if __name__ == "__main__":
