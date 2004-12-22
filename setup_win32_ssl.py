8b408a3584ac47ab6e8cefd06ac0a3a2911eba11
mfx <mfx>
2004-12-22 14:28:07 +0000

    Prepared for 7.12.3 release.

--- setup_win32_ssl.py
+++ setup_win32_ssl.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # vi:ts=4:et
-# $Id: setup_win32_ssl.py,v 1.23 2004/10/19 16:54:07 mfx Exp $
+# $Id: setup_win32_ssl.py,v 1.24 2004/12/22 14:28:08 mfx Exp $
 
 import os, sys, string
 assert sys.platform == "win32", "Only for building on Win32 with SSL and zlib"
 
 
-CURL_DIR = r"c:\src\build\pycurl\curl-7.12.2-ssl"
-OPENSSL_DIR = r"c:\src\build\pycurl\openssl-0.9.7d"
+CURL_DIR = r"c:\src\build\pycurl\curl-7.12.3-ssl"
+OPENSSL_DIR = r"c:\src\build\pycurl\openssl-0.9.7e"
 sys.argv.insert(1, "--curl-dir=" + CURL_DIR)
 
 from setup import *
@@ -19,8 +19,8 @@ setup_args["name"] = "pycurl-ssl"
 for l in ("libeay32.lib", "ssleay32.lib",):
     ext.extra_objects.append(os.path.join(OPENSSL_DIR, "out32", l))
 ext.extra_objects.append(r"c:\src\pool\zlib-1.2.2\pool\win32\vc6\zlib.lib")
-ext.extra_objects.append(r"c:\src\pool\c-ares-20041018\pool\win32\vc6\ares.lib")
-ext.extra_objects.append(r"c:\src\pool\libidn-0.5.8\pool\win32\vc6\idn.lib")
+ext.extra_objects.append(r"c:\src\pool\c-ares-20041212\pool\win32\vc6\ares.lib")
+ext.extra_objects.append(r"c:\src\pool\libidn-0.5.12\pool\win32\vc6\idn.lib")
 
 
 if __name__ == "__main__":
