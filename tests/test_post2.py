f9508ec4600d268e25d1598dfb78eb9208766caa
kjetilja <kjetilja>
2005-03-03 10:00:40 +0000

    - test the new curlform support

--- tests/test_post2.py
+++ tests/test_post2.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # vi:ts=4:et
-# $Id: test_post2.py,v 1.12 2004/04/30 15:12:28 kjetilja Exp $
+# $Id: test_post2.py,v 1.13 2005/03/03 10:00:40 kjetilja Exp $
 
 import pycurl
 
-
-pf = [('field1', 'this is a test using httppost & stuff'), ('field2', 'value2')]
+pf = [('field1', 'this is a test using httppost & stuff'),
+      ('field2', (pycurl.FORM_FILE, 'test_post.py', pycurl.FORM_FILE, 'test_post2.py')),
+      ('field3', (pycurl.FORM_CONTENTS, 'this is wei\000rd, but null-bytes are okay'))
+     ]
 
 c = pycurl.Curl()
 c.setopt(c.URL, 'http://www.contactor.se/~dast/postit.cgi')
