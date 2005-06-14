fa04765457ef9b1b3a6108322df474468946069a
kjetilja <kjetilja>
2005-06-14 13:43:26 +0000

    - ready for 7.14.0

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # vi:ts=4:et
-# $Id: setup.py,v 1.124 2005/04/06 13:05:32 kjetilja Exp $
+# $Id: setup.py,v 1.125 2005/06/14 13:43:26 kjetilja Exp $
 
 """Setup script for the PycURL module distribution."""
 
 PACKAGE = "pycurl"
 PY_PACKAGE = "curl"
-VERSION = "7.13.2"
+VERSION = "7.14.0"
 
 import glob, os, re, sys, string
 import distutils
@@ -63,7 +63,7 @@ if sys.platform == "win32":
     # Windows users have to configure the CURL_DIR path parameter to match
     # their cURL source installation.  The path set here is just an example
     # and thus unlikely to match your installation.
-    CURL_DIR = r"c:\src\build\pycurl\curl-7.13.2"
+    CURL_DIR = r"c:\src\build\pycurl\curl-7.14.0"
     CURL_DIR = scan_argv("--curl-dir=", CURL_DIR)
     print "Using curl directory:", CURL_DIR
     assert os.path.isdir(CURL_DIR), "please check CURL_DIR in setup.py"
--- setup_win32_ssl.py
+++ setup_win32_ssl.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # vi:ts=4:et
-# $Id: setup_win32_ssl.py,v 1.29 2005/04/12 03:39:46 mfx Exp $
+# $Id: setup_win32_ssl.py,v 1.30 2005/06/14 13:43:34 kjetilja Exp $
 
 import os, sys, string
 assert sys.platform == "win32", "Only for building on Win32 with SSL and zlib"
 
 
-CURL_DIR = r"c:\src\build\pycurl\curl-7.13.2-ssl"
+CURL_DIR = r"c:\src\build\pycurl\curl-7.14.0-ssl"
 OPENSSL_DIR = r"c:\src\build\pycurl\openssl-0.9.7g"
 sys.argv.insert(1, "--curl-dir=" + CURL_DIR)
 
