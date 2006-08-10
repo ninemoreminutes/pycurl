6a5141e1842b32a1bf424870c62274446365a9c5
kjetilja <kjetilja>
2006-08-10 11:41:16 +0000

    7.15.5

--- setup_win32_ssl.py
+++ setup_win32_ssl.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # vi:ts=4:et
-# $Id: setup_win32_ssl.py,v 1.36 2006/07/03 13:16:00 kjetilja Exp $
+# $Id: setup_win32_ssl.py,v 1.37 2006/08/10 11:41:16 kjetilja Exp $
 
 import os, sys, string
 assert sys.platform == "win32", "Only for building on Win32 with SSL and zlib"
 
 
-CURL_DIR = r"c:\src\build\pycurl\curl-7.15.4-ssl"
+CURL_DIR = r"c:\src\build\pycurl\curl-7.15.5-ssl"
 OPENSSL_DIR = r"c:\src\build\pycurl\openssl-0.9.7g"
 sys.argv.insert(1, "--curl-dir=" + CURL_DIR)
 
